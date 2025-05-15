8e7b002b06acc684b339de473ab2d88774bc8266
DuscraperRn <duscraper@gmail.com>
2025-05-15 15:28:39 +0000

    Add8

--- vars/customer.groovy
+++ vars/customer.groovy
@@ -2,6 +2,7 @@ def customers = ['Stacy', 'Ajay','Alexy','Vladmir','Li']
 def call(){
 	sh 'echo "Library has been imported successfully." '
 }
-def getList(){
-	return customers
+
+def getCustomers() {
+    return customers
 }
