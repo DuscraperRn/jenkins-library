45936969570657d41f7b5421c769776feabcb9ed
DuscraperRn <duscraper@gmail.com>
2025-05-16 07:54:35 +0000

    Add15

---
+++ vars/cluster.groovy
@@ -0,0 +1,11 @@
+def call(){
+	def cluster_list = ['eastus-01','westeurope01','eastus02','northeurope01']
+	return [
+		msg: {
+			echo "Initializing cluster list..."
+		}
+		getCluster: {
+			return cluster_list
+		}
+	]
+}
