fa3f031c2105800a567b34340e55b8ee41d17e18
DuscraperRn <duscraper@gmail.com>
2025-05-16 06:27:52 +0000

    Add14

--- vars/customer.groovy
+++ vars/customer.groovy
@@ -1,12 +1,16 @@
 
 def call() {
     def asset = ['Stacy', 'Ajay','Alexy','Vladmir','Li']
+    def env = ['ns001','ns002','alx01','vld004','stcy01']
     return [
         greet: {
             sh 'echo "Library has been imported successfully."'
         },
         getList: {
             return asset
-        }
+        },
+	getEnv: {
+	    return env
+	}
     ]
 }
