80a9301087a756b4814774d425d628093754f5eb
DuscraperRn <duscraper@gmail.com>
2025-05-17 11:11:12 +0000

    50

--- vars/customer.groovy
+++ vars/customer.groovy
@@ -1,7 +1,7 @@
 
 def call() {
-    def asset = ['Stacy', 'Ajay','Alexy','Vladmir','Li','Seimens','Nokia','dsdd']
-    def env = ['ns001','ns002','alx01','vld004','stcy01','seim','noki','mkm']
+    def asset = ['Stacy', 'Ajay','Alexy','Vladmir','Li','Seimens','Nokia','dsdd','Samsung']
+    def env = ['ns001','ns002','alx01','vld004','stcy01','seim','noki','mkm','ssng']
     return [
         greet: {
             sh 'echo "Library has been imported successfully."'
